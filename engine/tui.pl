8d73f5d9aa5f04d9560877a9c3b656995023f869
Omar Brikaa <brikaaomar@gmail.com>
2023-05-19 15:39:58 +0300

    TUI

--- engine/tui.pl
+++ engine/tui.pl
@@ -9,6 +9,11 @@ print_list([]).
 print_list([X | Xs]) :-
     write(X), nl, print_list(Xs).
 announce_state(State) :-
+    [X | _] = State,
+    length(X, Columns),
+    Columns1 is Columns - 1,
+    findall(Y, between(0, Columns1, Y), ColumnsIndicator),
+    write(ColumnsIndicator), nl,
     print_list(State), nl.
 
 announce_winner(X) :-
@@ -17,20 +22,44 @@ announce_winner(X) :-
 announce_invalid_move(Move) :-
     write("Invalid move: "), write(Move), nl.
 
+choose_color(1, Color, OppositeColor) :- red_piece(Color), blue_piece(OppositeColor).
+choose_color(2, Color, OppositeColor) :- blue_piece(Color), red_piece(OppositeColor).
+
+choose_algo_util(1, minimax_algo_util).
+choose_algo_util(2, abp_algo_util).
+
+choose_turn(1, human).
+choose_turn(2, computer).
+
 main(Winner) :-
-    form_board(4, 4, Board),
-    blue_piece(Blue),
-    red_piece(Red),
+    write('Rows'), nl,
+    read(Rows),
+    write('Columns'), nl,
+    read(Columns),
+    form_board(Rows, Columns, Board),
+    write('Human color:'), nl, write('1. Red'), nl, write('2. Blue'), nl,
+    read(ColorResponse),
+    choose_color(ColorResponse, Color, OppositeColor),
     Players = _{
-        human: Blue,
-        computer: Red
+        human: Color,
+        computer: OppositeColor
     },
-    state_util(4, StateUtil),
+    write('Consecutive pieces for winning:'), nl,
+    read(ConsecutivePieces),
+    state_util(ConsecutivePieces, StateUtil),
     IOUtil = _{
         get_move: get_move,
         announce_winner: announce_winner,
         announce_state: announce_state,
         announce_invalid_move: announce_invalid_move
     },
-    abp_algo_util(StateUtil, 4, AlgoUtil),
-    play(Board, human, Players, StateUtil, IOUtil, AlgoUtil, Winner).
+    write('Computer algorithm'), nl, write('1. Minimax'), nl, write('2. Alpha-beta pruning'), nl,
+    read(AlgorithmResponse),
+    choose_algo_util(AlgorithmResponse, AlgoUtilBuilder),
+    write('Computer max plies look-ahead:'), nl,
+    read(MaxPlies),
+    call(AlgoUtilBuilder, StateUtil, MaxPlies, AlgoUtil),
+    write('Does human play first?'), nl, write('1. Yes'), nl, write('2. No'), nl,
+    read(TurnResponse),
+    choose_turn(TurnResponse, Turn),
+    play(Board, Turn, Players, StateUtil, IOUtil, AlgoUtil, Winner).
